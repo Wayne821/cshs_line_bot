0d56add89157fa61714eac3e7aa5d8a1603a5b8a
Wayne821 <148313182+Wayne821@users.noreply.github.com>
2023-11-29 14:47:47 +0800

    Update S210711.py

--- student_modules/S210711.py
+++ student_modules/S210711.py
@@ -1,11 +1,11 @@
 def process(message: str) -> None:
     if message == "debug":
         return message
-    if message == "資訊社":
+    if message == "烤肉":
         return "讚"
     if message == "資訊老師":
-        return "帥"
-    if message == "資訊社的同學":
-        return "棒"
-    if message == "資訊社的電腦":
-        return "不好說"
+        return "超級帥"
+    if message == "棒棒糖":
+        return "好粗"
+    if message == "冠穎喜歡誰":
+        return "蜜蜂公主"
